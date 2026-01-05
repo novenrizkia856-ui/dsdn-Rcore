9502d36427bd98df1de7d3d35b972621116bbc87
Noven Rizkia <novenrizkia.8.5.6@gmail.com>
2026-01-05 15:13:07 +0700

    Menghapus file tidak berguna

--- crates/proto/build.rs
+++ crates/proto/build.rs
@@ -1,8 +1,3 @@
-// crates/proto/build.rs
-// Dummy build script agar cargo tidak error.
-// Tidak perlu generate proto di sini, karena build.rs di storage yang akan melakukannya.
-
 fn main() {
-    // kalau nanti file proto diubah, ini memastikan cargo re-run build.
     println!("cargo:rerun-if-changed=proto/api.proto");
 }
